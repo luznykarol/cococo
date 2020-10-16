c9ae0c158c74939c9f53bc26a7b96f9974356f0d
Karol Rudzinski <karol.rudzinski1990@gmail.com>
2020-10-16 18:28:09 +0200

    config change

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,19 +37,20 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/static/img`,
-        name: 'images',
-      },
-    },
+
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/static/img`,
         name: 'uploads',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static/img`,
+        name: 'images',
       },
     },
     {
